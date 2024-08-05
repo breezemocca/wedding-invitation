e5e3f4230b2cfb13d9ff8490a1274494a14fedaf
breezemocca <37162650+breezemocca@users.noreply.github.com>
2024-08-05 20:28:54 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,20 +44,20 @@ export default function Home() {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:type" content="website" />
-        <meta content="○○○❤○○○ 결혼식에 초대합니다" name="Title" />
+        <meta content="심명훈❤장혜원 결혼식에 초대합니다" name="Title" />
         <meta
-          content="○○○○년 ○○월 ○○일 ○요일 오전 ○○시 ○○분"
+          content="2024년 12월 22일 일요일 오전 11시 00분"
           name="Description"
         />
-        <meta content="○○○○년 ○○월 ○○일 ○요일 오전 ○○시 ○○분" name="Keyword" />
-        <meta property="og:title" content="○○○❤○○○ 결혼식에 초대합니다" />
+        <meta content="2024년 12월 22일 일요일 오전 11시 00분" name="Keyword" />
+        <meta property="og:title" content="심명훈❤장혜원 결혼식에 초대합니다" />
         <meta
           property="og:description"
-          content="○○○○년 ○○월 ○○일 ○요일 오전 ○○시 ○○분"
+          content="2024년 12월 22일 일요일 오전 11시 00분"
         />
         <meta
           property="og:url"
-          content="https://kyuhyuk.kr/wedding-invitation"
+          content="https://breezemocca.github.io"
         />
         <meta name="theme-color" content="#BCAAA4" />
         <title>○○○❤○○○ 결혼식에 초대합니다</title>
